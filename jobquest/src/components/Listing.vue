7b5fee509ae451d1560d9f2dbdcb783366b72e0b
Randi Mohorović <randi.mohorovic@gmail.com>
2023-12-02 18:41:20 +0100

    v-for loop
    loop petlja za job-listing

--- jobquest/src/components/Listing.vue
+++ jobquest/src/components/Listing.vue
@@ -1,20 +1,24 @@
 <template>
 
   <div class="card text-center">
-       <div class="card-header">Featured</div>
-        <div class="card-body p-0"> </div>
-        <img  class="card-img-top" src="https://picsum.photos/1080">
+       <div class="card-header">Featured </div>
+        <div class="card-body p-0"></div>
+        <img  class="card-img-top" :src="info"/> 
+        
         <div class="card-footer text-body-secondary">2 days ago</div>
    </div>
 
 </template>
 
 <script>
+import { initCustomFormatter } from 'vue';
+
 export default {
+  props: ['info'],  // elementi aaraya su nazivi parametra koje vue.js komponenta prima, parametarar "info"
   name: 'ListingJob',
 
   };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 
--- jobquest/src/views/HomeView.vue
+++ jobquest/src/views/HomeView.vue
@@ -26,12 +26,11 @@
 
   <!-- cards -->
   <div class="row">
-    <div class="col-2">filtri/linkovi</div>
-    <div class="col-7"> <Listing-Job />
-      <Listing-Job />
-      <Listing-Job />
+    <div class="col-3">filtri/linkovi</div>
+       <div class="col-7">   <!--  pozivas komponentu listin job  -->
+      <Listing-Job v-for="x in listing" :key="x" :info="x" />   <!-- x element u data- jobs:, key: "x"pozivas vrijednost x el.   --> 
     </div>
-    <div class="col-3">blok</div>
+    <div class="col-2"> empty - blok {{ lista }}</div>
   </div>
 </template>
 
@@ -40,8 +39,24 @@
 //
 import ListingJob from '@/components/Listing.vue';
 
+let listing = [];
+
+listing = [
+'https://picsum.photos/id/1/400/400',
+'https://picsum.photos/id/2/400/400',
+'https://picsum.photos/id/3/400/400',
+
+];
+
 export default {
   name: 'HomeView',
+  data: function () {
+    return {
+      // kljuc : vrijednost 
+       // lista : [1,2,3,"four"], // zelim imat podatke koji ce rec koji su dostupni poslovi
+       listing: listing,
+    };
+  },
   components: {
   ListingJob,
   },
