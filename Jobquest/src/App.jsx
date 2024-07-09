aa004efae1ab7384b7834263a49755937c7e98ed
Randi Mohorović <92921082+randimohorovic@users.noreply.github.com>
2024-07-09 19:20:35 +0200

    Route set up
     views- napravljene,
    rute određene otprilike

--- Jobquest/src/App.jsx
+++ Jobquest/src/App.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import Login from "./views/Login";
+import SignUp from "./views/SignUp";
+import About from "./views/About";
+import Profile from "./views/Profile";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="log-in" element={<Login />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="about" element={<About />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
