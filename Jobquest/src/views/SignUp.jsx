bbe889795dcab66dcd6de99742c51628edd32a17
Randi Mohorović <92921082+randimohorovic@users.noreply.github.com>
2024-07-20 13:31:05 +0200

    Update SignUp.jsx
    dodat funkcije na signup

--- Jobquest/src/views/SignUp.jsx
+++ Jobquest/src/views/SignUp.jsx
@@ -1,68 +1,84 @@
 import { useState } from "react";
+import { Label, TextInput } from "flowbite-react";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-  const [userData, setFormData] = useState({});
-  const [error, setError] = useState({});
-  const navigate = useNavigate();
-  const handleChange = (e) => {
-    setFormData({
-      ...userData,
-      [e.target.id]: e.target.value,
-    });
-  };
-  console.log(userData);
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const res = await fetch("/backend/auth/signup", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(userData),
-    });
-    const data = await res.json();
-    if (data.success === false) {
-      setError(data.message);
-      return;
-    }
+  // const [userData, setFormData] = useState({});
+  // const [error, setError] = useState({});
+  // const navigate = useNavigate();
+  // const onchange = (e) => {
+  //   setFormData({
+  //     ...userData,
+  //     [e.target.id]: e.target.value,
+  //   });
+  // };
+  // console.log(userData);
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const res = await fetch("/backend/auth/signup", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify(userData),
+  //   });
+  //   const data = await res.json();
+  //   if (data.success === false) {
+  //     setError(data.message);
+  //     return;
+  //   }
 
-    console.log(data);
-  };
-  console.log(userData);
+  //   console.log(data);
+  // };
+  // console.log(userData);
   return (
-    <div className="p-3 max-w-md mx-auto">
-      <h1 className="text3xl text-center font-sans my-3">Kreiraj Račun</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-6">
-        <input
-          type="text"
-          placeholder="korisničko ime"
-          className="border p-3 rounded-lg"
-          id="username"
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          className="border p-3 rounded-lg"
-          id="email"
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          placeholder="Lozinka "
-          className="border p-3 rounded-lg"
-          id="password"
-          onChange={handleChange}
-        />
-        <button className="bg-cyan-600 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-65">
-          Kreiraj račun
-        </button>
-      </form>
-      <div className="flex gap-1 mt-3">
-        <p>Imate li račun? </p>
-        <Link to={"/sign-in"}>
-          {" "}
-          <span className="text-cyan-600">Prijavi se</span>
-        </Link>
+    <div className="main-h-screen mt-20">
+      <div
+        className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center
+      gap-5"
+      >
+        {/* ljeva strana */}
+        <div className="flex-1">
+          <Link to="/" className="text-4xl font-bold">
+            <span className="px-2 py-1 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg text-white">
+              Jobquest
+            </span>
+          </Link>
+          <p className="text-sm mt-5">Ovjde možete kreirati svoj račun!</p>
+        </div>
+
+        {/* desna strana*/}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Korisničko ime" />
+              <TextInput
+                type="text"
+                placeholder="Korisničko ime"
+                id="username"
+              />
+            </div>
+            <div>
+              <Label value="Email" />
+              <TextInput type="text" placeholder="@" id="email" />
+            </div>
+            <div>
+              <Label value="Lozinka" />
+              <TextInput type="text" placeholder="Lozinka" id="password" />
+            </div>
+            <button
+              type="submit"
+              class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800  hidden sm:inline font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            >
+              Kreiraj račun
+            </button>
+          </form>
+          <div className="flex gap-1 mt-3 text-sm font-bold">
+            <p>Imate li račun? </p>
+            <Link to={"/sign-in"}>
+              {" "}
+              <span className="text-cyan-600">Prijavi se</span>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
