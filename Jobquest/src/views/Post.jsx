cc3d9d5b6f464f5d9a87a9e503138d813ed7a9b5
Randi Mohorović <92921082+randimohorovic@users.noreply.github.com>
2024-08-23 20:02:56 +0200

    Post.jsx
    dorađen post.jsx,
    popravljen dark mode... sintaksa...

--- Jobquest/src/views/Post.jsx
+++ Jobquest/src/views/Post.jsx
@@ -1,5 +1,41 @@
+import { TextInput, Select } from "flowbite-react";
 import React from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 export default function Post() {
-  return <div>post</div>;
+  //mx auto centar dovest
+  return (
+    <div className="p-3 max-w-3xl mx-auto min-h-screen">
+      <h1 className="text-center text-3xl my-7 font-semibold">
+        Kreiraj objavu
+      </h1>
+      <form className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 sm:flex-row justify-between">
+          <TextInput
+            type="text"
+            placeholder="Naslov objave"
+            require
+            id="naslov"
+            className="flex-1"
+          />
+          <Select>
+            <option value="empty">Odaberi kategoriju</option>
+            <option value="IT">IT</option>
+            <option value="Pomoćni poslovi">Pomoćni poslovi</option>
+            <option value="Administrativni poslovi">
+              Administrativni poslovi
+            </option>
+          </Select>
+        </div>
+        <div className="flex gap-4 items-center justify-between border-4 border-cyan-500 border-dotted p-3"></div>
+        <ReactQuill
+          theme="snow"
+          placeholder="Opis..."
+          className="h-72 mb-12 "
+        />
+        ;
+      </form>
+    </div>
+  );
 }
