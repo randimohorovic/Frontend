f50c59680549733e7c7887f14ad3c6e4a3359f8c
Randi Mohorović <92921082+randimohorovic@users.noreply.github.com>
2024-08-24 13:41:31 +0200

    Post.jsx
    submit button, backedn ruta dodanai funkcionalnosti

--- Jobquest/src/views/Profile.jsx
+++ Jobquest/src/views/Profile.jsx
@@ -56,7 +56,7 @@ export default function profile() {
   };
   const dispatch = useDispatch();
   return (
-    <div className="main-h-screen mt-20">
+    <div className="main-h-screen mt-20 min-h-screen">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* Left side */}
         <div className="flex-1">
