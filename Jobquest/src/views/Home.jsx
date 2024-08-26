a1ffc3a050f5d9dd5abb4ab4814800bb249e4de3
Randi Mohorović <92921082+randimohorovic@users.noreply.github.com>
2024-08-26 18:18:37 +0200

    Update Home.jsx

--- Jobquest/src/views/Home.jsx
+++ Jobquest/src/views/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
         <h1 className="text-3xl font-bold lg:text-6xl">Ponuda poslova</h1>
         <p className="text-gray-500 text-xs sm:text-sm">
-          Pronađi posao preko Student servisa
+          Pronađi posao preko Jobquest aplikacije
         </p>
         <Link
           to="/search"
